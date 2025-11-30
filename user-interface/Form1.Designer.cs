2673a1394e3a57c20690f5ea62421af983a5aa49
chien2005-obito <chien200605@gmail.com>
2025-11-30 23:31:46 +0700

    User-Interface

---
+++ user-interface/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace pwm_user_interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serCom = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnCONNECT = new System.Windows.Forms.Button();
+            this.btnEXIT = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbSpeed = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(234, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PWM USER INTERFACE";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(239, 150);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(298, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // btnCONNECT
+            // 
+            this.btnCONNECT.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnCONNECT.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnCONNECT.Location = new System.Drawing.Point(131, 293);
+            this.btnCONNECT.Name = "btnCONNECT";
+            this.btnCONNECT.Size = new System.Drawing.Size(163, 48);
+            this.btnCONNECT.TabIndex = 2;
+            this.btnCONNECT.Text = "CONNECT";
+            this.btnCONNECT.UseVisualStyleBackColor = true;
+            this.btnCONNECT.Click += new System.EventHandler(this.btnCONNECT_Click);
+            // 
+            // btnEXIT
+            // 
+            this.btnEXIT.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnEXIT.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.btnEXIT.Location = new System.Drawing.Point(503, 293);
+            this.btnEXIT.Name = "btnEXIT";
+            this.btnEXIT.Size = new System.Drawing.Size(113, 48);
+            this.btnEXIT.TabIndex = 3;
+            this.btnEXIT.Text = "EXIT";
+            this.btnEXIT.UseVisualStyleBackColor = true;
+            this.btnEXIT.Click += new System.EventHandler(this.btnEXIT_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(152, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "PWM:";
+            // 
+            // lbSpeed
+            // 
+            this.lbSpeed.AutoSize = true;
+            this.lbSpeed.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbSpeed.Location = new System.Drawing.Point(543, 150);
+            this.lbSpeed.Name = "lbSpeed";
+            this.lbSpeed.Size = new System.Drawing.Size(73, 25);
+            this.lbSpeed.TabIndex = 5;
+            this.lbSpeed.Text = "Speed";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnEXIT);
+            this.Controls.Add(this.btnCONNECT);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serCom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btnCONNECT;
+        private System.Windows.Forms.Button btnEXIT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbSpeed;
+    }
+}
+
---
+++ user-interface/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace pwm_user_interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void btnCONNECT_Click(object sender, EventArgs e)
+        {
+            if (!serCom.IsOpen)
+            {
+                btnCONNECT.Text = "DISCONNECT";
+                serCom.PortName = "COM5";
+                serCom.BaudRate = Convert.ToInt32("115200");
+
+                serCom.Open();
+            }
+            else
+            {
+                btnCONNECT.Text = "CONNECT";
+                serCom.Write("000");
+                serCom.Close();
+            }
+        }
+
+        private void btnEXIT_Click(object sender, EventArgs e)
+        {
+            serCom.Close();
+            Application.Exit();
+        }
+
+        string value;
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            value = trackBar1.Value.ToString("D3");
+
+            int pwmValue = int.Parse(value);
+
+            lbSpeed.Text = "Speed: " + pwmValue.ToString();
+
+            serCom.Write(value); 
+
+        }
+    }
+}
