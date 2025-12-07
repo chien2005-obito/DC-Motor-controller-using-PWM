4385bece7e545cf456d5dc202367c518d816a5aa
chien2005-obito <chien200605@gmail.com>
2025-12-07 23:20:56 +0700

    Upload code first time!

---
+++ user-interface/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace pwm_user_interface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serCom = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.btnCONNECT = new System.Windows.Forms.Button();
+            this.btnEXIT = new System.Windows.Forms.Button();
+            this.lbSpeed = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbBaudrate = new System.Windows.Forms.ComboBox();
+            this.cmbCOM = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnBackward = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.txtSpeedValue = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serCom
+            // 
+            this.serCom.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serCOM_DataReceived);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(65, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PWM USER INTERFACE";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(447, 38);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(398, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // btnCONNECT
+            // 
+            this.btnCONNECT.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnCONNECT.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnCONNECT.Location = new System.Drawing.Point(42, 123);
+            this.btnCONNECT.Name = "btnCONNECT";
+            this.btnCONNECT.Size = new System.Drawing.Size(163, 48);
+            this.btnCONNECT.TabIndex = 2;
+            this.btnCONNECT.Text = "CONNECT";
+            this.btnCONNECT.UseVisualStyleBackColor = true;
+            this.btnCONNECT.Click += new System.EventHandler(this.btnCONNECT_Click);
+            // 
+            // btnEXIT
+            // 
+            this.btnEXIT.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnEXIT.ForeColor = System.Drawing.Color.Crimson;
+            this.btnEXIT.Location = new System.Drawing.Point(289, 123);
+            this.btnEXIT.Name = "btnEXIT";
+            this.btnEXIT.Size = new System.Drawing.Size(113, 48);
+            this.btnEXIT.TabIndex = 3;
+            this.btnEXIT.Text = "EXIT";
+            this.btnEXIT.UseVisualStyleBackColor = true;
+            this.btnEXIT.Click += new System.EventHandler(this.btnEXIT_Click);
+            // 
+            // lbSpeed
+            // 
+            this.lbSpeed.AutoSize = true;
+            this.lbSpeed.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbSpeed.Location = new System.Drawing.Point(536, 87);
+            this.lbSpeed.Name = "lbSpeed";
+            this.lbSpeed.Size = new System.Drawing.Size(124, 25);
+            this.lbSpeed.TabIndex = 5;
+            this.lbSpeed.Text = "Duty cycle:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbBaudrate);
+            this.groupBox1.Controls.Add(this.cmbCOM);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox1.Location = new System.Drawing.Point(42, 209);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 131);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM CONFIGURATION";
+            // 
+            // cmbBaudrate
+            // 
+            this.cmbBaudrate.FormattingEnabled = true;
+            this.cmbBaudrate.Location = new System.Drawing.Point(174, 81);
+            this.cmbBaudrate.Name = "cmbBaudrate";
+            this.cmbBaudrate.Size = new System.Drawing.Size(101, 30);
+            this.cmbBaudrate.TabIndex = 9;
+            this.cmbBaudrate.SelectedIndexChanged += new System.EventHandler(this.cmbBaudrate_SelectedIndexChanged);
+            // 
+            // cmbCOM
+            // 
+            this.cmbCOM.FormattingEnabled = true;
+            this.cmbCOM.Location = new System.Drawing.Point(174, 36);
+            this.cmbCOM.Name = "cmbCOM";
+            this.cmbCOM.Size = new System.Drawing.Size(101, 30);
+            this.cmbCOM.TabIndex = 0;
+            this.cmbCOM.SelectedIndexChanged += new System.EventHandler(this.cmbCOM_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Baudrate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "COM";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnBackward);
+            this.groupBox2.Controls.Add(this.btnForward);
+            this.groupBox2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox2.Location = new System.Drawing.Point(473, 232);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(345, 108);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SWITCH ";
+            // 
+            // btnBackward
+            // 
+            this.btnBackward.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnBackward.Location = new System.Drawing.Point(189, 35);
+            this.btnBackward.Name = "btnBackward";
+            this.btnBackward.Size = new System.Drawing.Size(118, 53);
+            this.btnBackward.TabIndex = 8;
+            this.btnBackward.Text = "Backward";
+            this.btnBackward.UseVisualStyleBackColor = true;
+            this.btnBackward.Click += new System.EventHandler(this.btnBackward_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnForward.Location = new System.Drawing.Point(41, 35);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(118, 53);
+            this.btnForward.TabIndex = 7;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = true;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // txtSpeedValue
+            // 
+            this.txtSpeedValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSpeedValue.Location = new System.Drawing.Point(25, 29);
+            this.txtSpeedValue.Name = "txtSpeedValue";
+            this.txtSpeedValue.ReadOnly = true;
+            this.txtSpeedValue.Size = new System.Drawing.Size(162, 36);
+            this.txtSpeedValue.TabIndex = 13;
+            this.txtSpeedValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtSpeedValue.TextChanged += new System.EventHandler(this.txtSpeedValue_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.txtSpeedValue);
+            this.groupBox3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.groupBox3.Location = new System.Drawing.Point(473, 123);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(201, 81);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Rotation speed";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnStop.ForeColor = System.Drawing.Color.Orange;
+            this.btnStop.Location = new System.Drawing.Point(692, 140);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(113, 48);
+            this.btnStop.TabIndex = 15;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 377);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbSpeed);
+            this.Controls.Add(this.btnEXIT);
+            this.Controls.Add(this.btnCONNECT);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serCom;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button btnCONNECT;
+        private System.Windows.Forms.Button btnEXIT;
+        private System.Windows.Forms.Label lbSpeed;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbBaudrate;
+        private System.Windows.Forms.ComboBox cmbCOM;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnBackward;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.TextBox txtSpeedValue;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ user-interface/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace pwm_user_interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            String[] Baud_Rate = { "1200", "2400", "4800", "9600", "115200" };
+            cmbBaudrate.Items.AddRange(Baud_Rate);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCOM.DataSource = SerialPort.GetPortNames();
+            cmbCOM.Text = "Undefined";
+            cmbBaudrate.Text = "Undefined";
+        }
+
+
+        private void btnCONNECT_Click(object sender, EventArgs e)
+        {
+          if ((cmbCOM.Text == "Undefined") || (cmbBaudrate.Text == "Undefined"))
+            {
+                MessageBox.Show("Please set configuration first");
+                return;
+            }
+            try
+            {
+                if (!serCom.IsOpen)
+                {
+                    btnCONNECT.Text = "DISCONNECT";
+                    serCom.PortName = cmbCOM.Text;
+                    serCom.BaudRate = Convert.ToInt32(cmbBaudrate.Text);
+                    serCom.Open();
+                }
+                else
+                {
+                    serCom.Write("000");
+                    trackBar1.Value = 0;
+                    lbSpeed.Text = "Duty cycle: 0%";
+                    btnCONNECT.Text = "CONNECT";
+                    serCom.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eror " + ex.Message);
+            }
+        }
+
+        private void btnEXIT_Click(object sender, EventArgs e)
+        {
+            if (serCom.IsOpen)
+            {
+                serCom.Write("000");
+                serCom.Close();
+            }
+            Application.Exit();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (serCom.IsOpen)
+            {
+                string value;
+                value = trackBar1.Value.ToString("D3");
+
+                int pwmValue = int.Parse(value);
+
+                lbSpeed.Text = "Duty cycle: " + pwmValue.ToString() + "%";
+
+                serCom.Write(value);
+            }
+            else
+            {
+                MessageBox.Show("Please connect Com port!");
+            }
+        }
+
+        private void cmbCOM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbBaudrate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serCom.IsOpen)
+            {
+                serCom.Write("000");
+                serCom.Close();
+            }
+        }
+
+        private void btnBackward_Click(object sender, EventArgs e)
+        {
+            if (serCom.IsOpen)
+            {
+                serCom.Write("NGH"); 
+            }
+            else
+            {
+                MessageBox.Show("Please connect Com port!");
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (serCom.IsOpen)
+            {
+                serCom.Write("THN"); 
+            }
+            else
+            {
+                MessageBox.Show("Please connect Com port!");
+            }
+        }
+
+        private void serCOM_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                if (serCom.IsOpen)
+                {                       
+                    string dataReceived = serCom.ReadLine();
+
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        txtSpeedValue.Text = dataReceived + " RPS";
+                    }));
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (serCom.IsOpen)
+            {
+                
+                serCom.Write("000");                
+                trackBar1.Value = 0;
+                lbSpeed.Text = "Duty cycle: 0%";
+            }
+            else
+            {
+                MessageBox.Show("Com port not connected!");
+            }
+        }
+
+        private void txtSpeedValue_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
